af4a2640bc46e6e5ed7e40e1619020dd02f0c07b
joseda77 <tello123@hotmail.es>
2018-09-22 11:45:42 -0500

    Dojo de heroku con queryString

--- index.php
+++ index.php
@@ -4,5 +4,11 @@
 </head>
 <body>
     <?php echo '<p>Hola mundo Jose</p>';?>
+    <?php  
+    $name=$_GET['name'];
+    $apellido=$_GET['apellido'];
+    echo "<p>Nombre \$name : $name";
+    echo "<p>Aepllido \$apellido : $apellido";
+    ?>
 </body>
 </html>
